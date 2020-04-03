d8794f268561fbb4ff35c6a464b042f6beb6c736
Hrannar19 <hrannar19@ru.is>
2020-04-03 11:16:57 +0000

    breytti portinu

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
-	console.log('Server is listening on port *:3000');
+http.listen(8080, function(){
+	console.log('Server is listening on port *:8080');
 });
 
